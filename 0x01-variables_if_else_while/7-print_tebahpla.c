ce09e88d21c424d4852d47c81e62dc8bd0336fd1
Ubong007 <elvisbrave26@gmail.com>
2023-07-16 16:06:54 -0700

    More

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the lowercase alphamet in reverse.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+	char letter;
+	for (letter = 'z'; letter >= 'a'; letter--)
+	         putcher(letter);
+
+	putcher(*\n*);	 
