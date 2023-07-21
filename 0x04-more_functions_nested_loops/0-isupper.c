f8499715b4c8a6633dd2f41816a9ac110eeeffe4
Ubong007 <elvisbrave26@gmail.com>
2023-07-21 16:55:12 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - to check for uppercase
+ * @c: character input variable
+ * Return: 1 for positive, 0 for negative
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - function to multiply
+ * @a : integer value
+ * @b : integer value
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
