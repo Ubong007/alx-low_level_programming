44b643449cf5f9c89a8ed2b52945f063cd4958aa
Ubong Effiong <elvisbrave26@gmail.com>
2023-08-04 14:23:11 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,4 @@
-#include < stdio.h >
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
@@ -11,16 +11,19 @@
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2, prod;
+        int num1, num2, prod;
 
-	if (argc != 3)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
-	prod = num1 * num2;
-	printf("%d\n", prod);
-	return (0);
+        if (argc != 3)
+        {
+                printf("Error\n");
+                return (1);
+        }
+
+        num1 = atoi(argv[1]);
+        num2 = atoi(argv[2]);
+        prod = num1 * num2;
+
+        printf("%d\n", prod);
+
+        return (0);
 }
