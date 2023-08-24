688c24612aefa5fb500a23fc121f17a73a564a9e
Ubong Effiong <elvisbrave26@gmail.com>
2023-08-24 16:25:16 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * first - Prints a sentence before the main function is executed.
+ * Description: This function is marked with the constructor attribute,
+ * it prints two sentences to the standard output.
+ */
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
