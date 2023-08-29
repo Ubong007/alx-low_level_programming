82eb25462b1583b18366475008ad88eaacacbc4a
Ubong Effiong <elvisbrave26@gmail.com>
2023-08-29 03:31:09 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,17 +7,15 @@
  *
  * Return: The number of nodes in the linked list.
  */
-
 size_t print_listint(const listint_t *h)
 {
- size_t num = 0;
+	size_t num = 0;
 
- while (h)
- {
- printf("%d\n", h->n);
- num++;
- h = h->next;
- }
-
- return (num);
+	while (h)
+	{
+		printf("%d\n", h->n);
+		num++;
+		h = h->next;
+	}
+	return (num);
 }
