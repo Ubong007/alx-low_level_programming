8e1f9aa633855e28254587df7eaf19b515277c07
Ubong007 <elvisbrave26@gmail.com>
2023-11-06 07:49:52 -0800

    0-hash task

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,10 +1,10 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - Creates a hash table
- * @size: Size of the array
+ * hash_table_create - creates a hash table
+ * @size: size of the array
  *
- * Return: Pointer to the newly created hash table
+ * Return: pointer to the newly created hash table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
